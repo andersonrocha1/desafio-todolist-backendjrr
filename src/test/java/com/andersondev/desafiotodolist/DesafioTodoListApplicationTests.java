c26d9032ac69f467620966336d8039566a8393d7
Anderson Rocha <anderson.tec.informacao@gmail.com>
2023-09-05 18:57:01 -0300

    Commit 1: Estruturando em camadas

---
+++ src/test/java/com/andersondev/desafiotodolist/DesafioTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersondev.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
